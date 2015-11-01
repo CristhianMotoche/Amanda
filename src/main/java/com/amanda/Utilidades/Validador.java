8d14337e2e6810d353d27f144169bfe499067673
CristhianMotoche <cristhian.motoche@gmail.com>
2015-10-31 19:40:37 -0500

    Añadir funcion para validación de cedula y pruebas.

--- src/main/java/com/amanda/Utilidades/Validador.java
+++ src/main/java/com/amanda/Utilidades/Validador.java
@@ -10,5 +10,41 @@ package com.amanda.Utilidades;
  * @author camm
  */
 public class Validador {
-    
+
+    public Validador() {
+    }
+
+    public boolean cedulaValida(String cedula){
+        if (cedula.length() > 10 || cedula.length() < 10) {
+            return false;
+        }
+        else{
+            int numeros [] = new int [10];
+            for (int i = 0; i < cedula.length(); i++) {
+                numeros[i] = (int) (cedula.charAt(i)) - 48;
+            }
+            return luhn(numeros);
+        }
+    }
+
+    public boolean luhn(int numeros[]){
+        int numVerificacion = numeros[numeros.length - 1];
+        int suma = 0;
+
+        for (int i = 0; i < numeros.length - 1; i+=2) {
+            numeros[i] = numeros[i]*2;
+            if(numeros[i] > 9){
+                numeros[i] -= 9;
+            }
+        }
+
+        for (int i = 0; i < numeros.length; i++) {
+            suma += numeros[i];
+        }
+
+        int resultado = 10 - suma%10;
+        resultado = (resultado < 10)? resultado : 0;
+
+        return (resultado == numVerificacion);
+    }
 }
---
+++ src/test/java/com/amanda/Utilidades/ValidadorTest.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.amanda.Utilidades;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author camm
+ */
+public class ValidadorTest {
+    @Test
+    public void testCedulaValida() {
+        System.out.println("Cedula con menos de 10 numeros no es valida");
+        Validador instance = new Validador();
+
+        String ci = "12345";
+
+        boolean expResult = false;
+        boolean result = instance.cedulaValida(ci);
+
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testCedulaValida2() {
+        System.out.println("Cedula con más de 10 numeros no es valida");
+        Validador instance = new Validador();
+
+        String ci = "12345678910";
+
+        boolean expResult = false;
+        boolean result = instance.cedulaValida(ci);
+
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testCedulaValida3() {
+        System.out.println("Cedula valida");
+        Validador instance = new Validador();
+
+        String ci = "1725651630";
+
+        boolean expResult = true;
+        boolean result = instance.cedulaValida(ci);
+
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testCedulaValida4() {
+        System.out.println("Cedula no valida");
+        Validador instance = new Validador();
+
+        String ci = "1725651631";
+
+        boolean expResult = false;
+        boolean result = instance.cedulaValida(ci);
+
+        assertEquals(expResult, result);
+    }
+}
