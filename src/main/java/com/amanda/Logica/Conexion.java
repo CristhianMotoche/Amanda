b2079e62eb1691382919313140cdc18240bcdc02
CristhianMotoche <cristhian.motoche@gmail.com>
2015-11-01 19:18:23 -0500

    Añadir metodos faltantes a las clases Tabla y Conexion

--- src/main/java/com/amanda/Logica/Conexion.java
+++ src/main/java/com/amanda/Logica/Conexion.java
@@ -15,17 +15,45 @@ public class Conexion {
     private Connection con;
     private String user = "amanda";
     private String pass = "amanda";
-    private String bd = "Amanda";
-    private String url = "jdbc:derby://localhost/Amanda" + bd;
-    
+    private String url = "jdbc:derby://localhost/Amanda";
+
     public Conexion() {
         con = null;
         try {
             Class.forName("org.apache.derby.jdbc.ClientDriver");
             con = DriverManager.getConnection(url, user, pass);
-            con.setSchema("Amanda");
+            con.setSchema("AMANDA");
         } catch (ClassNotFoundException | SQLException ex) {
             JOptionPane.showMessageDialog(null, ex);
         }
+    }
+
+    public Statement Crear(){
+        try {
+            return con.createStatement();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex);
+        }
+        return null;
+    }
+
+    public PreparedStatement preparar(String CadSQL){
+        try {
+            return con.prepareStatement(CadSQL);
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex);
+        }
+        return null;
+    }
+
+    public boolean Cerrar(){
+        boolean ret = true;
+        try {
+            con.close();
+        } catch (SQLException ex) {
+            ret = false;
+            JOptionPane.showMessageDialog(null, ex);
+        }
+        return ret;
     }
 }
--- src/main/java/com/amanda/Logica/Tabla.java
+++ src/main/java/com/amanda/Logica/Tabla.java
@@ -6,6 +6,8 @@
 package com.amanda.Logica;
 
 import com.amanda.Datos.Datos;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -13,8 +15,45 @@ import javax.swing.table.DefaultTableModel;
  * @author camm
  */
 public abstract class Tabla {
+    protected final Conexion con;
+    protected String cadSQL;
+    protected String nombreTabla;
+    protected ResultSet resultadoCrudo;
+
+    public Tabla(){
+        con = new Conexion();
+        cadSQL = "";
+        resultadoCrudo = null;
+    }
+
+    //protected ResultSet getResultadoCrudo() { return resultadoCrudo; }
+
+    protected void consultar(){
+        resultadoCrudo = null;
+        try {
+            Statement s = con.Crear();
+            s.execute(cadSQL);
+            resultadoCrudo = s.getResultSet();
+            con.Cerrar();
+        } catch (Exception e) { resultadoCrudo = null; }
+    }
+
+    protected DefaultTableModel obtenerTabla(String encabezado[]){
+        DefaultTableModel ret = null;
+        try {
+            consultar();
+            String regs[] = new String[encabezado.length];
+            while(resultadoCrudo.next()){
+                for (int i = 0; i < encabezado.length; i++) regs[i] = resultadoCrudo.getString(encabezado[i]);
+                ret.addRow(regs);
+            }
+            resultadoCrudo.first();
+        } catch (Exception e) { ret = null; }
+        return ret;
+    }
+
+    public abstract DefaultTableModel mostrar();
     public abstract boolean insertar(Datos dts);
-    public abstract boolean actualizar(Datos dts);
-    public abstract DefaultTableModel mostrar(Datos dts);
+    public abstract boolean editar(Datos dts);
     public abstract boolean eliminar(Datos dts);
 }
