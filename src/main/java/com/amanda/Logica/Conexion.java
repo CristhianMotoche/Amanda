e37cab840e7ef6775d68561ffaed570a47f5b50a
SantiagoYepez <santymatarata@gmail.com>
2015-11-04 18:02:04 -0500

    Cambios en la clase 'Conexion' y en 'Tabla'. La cagué.

--- src/main/java/com/amanda/Logica/Conexion.java
+++ src/main/java/com/amanda/Logica/Conexion.java
@@ -1,59 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.amanda.Logica;
+
 
 import java.sql.*;
 import javax.swing.JOptionPane;
-/**
- *
- * @author camm
- */
+
 public class Conexion {
     private Connection con;
-    private String user = "amanda";
-    private String pass = "amanda";
-    private String url = "jdbc:derby://localhost/Amanda";
-
+    
     public Conexion() {
         con = null;
         try {
             Class.forName("org.apache.derby.jdbc.ClientDriver");
-            con = DriverManager.getConnection(url, user, pass);
+            con = DriverManager.getConnection("jdbc:derby://localhost/Amanda; create = true; user = amanda; password = amanda");
             con.setSchema("AMANDA");
         } catch (ClassNotFoundException | SQLException ex) {
             JOptionPane.showMessageDialog(null, ex);
         }
     }
-
-    public Statement Crear(){
-        try {
-            return con.createStatement();
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, ex);
-        }
-        return null;
-    }
-
-    public PreparedStatement preparar(String CadSQL){
-        try {
-            return con.prepareStatement(CadSQL);
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, ex);
-        }
-        return null;
-    }
-
-    public boolean Cerrar(){
-        boolean ret = true;
-        try {
-            con.close();
-        } catch (SQLException ex) {
-            ret = false;
-            JOptionPane.showMessageDialog(null, ex);
-        }
-        return ret;
-    }
+    
+    public Connection obtenerConexion(){ return con; }
+    
 }
--- src/main/java/com/amanda/Logica/Tabla.java
+++ src/main/java/com/amanda/Logica/Tabla.java
@@ -1,57 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.amanda.Logica;
 
 import com.amanda.Datos.Datos;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.Connection;
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author camm
- */
 public abstract class Tabla {
-    protected final Conexion con;
+    protected final Connection con;
     protected String cadSQL;
-    protected String nombreTabla;
-    protected ResultSet resultadoCrudo;
-
+    public int totalregistros;
+    
     public Tabla(){
-        con = new Conexion();
+        con = (new Conexion()).obtenerConexion();
         cadSQL = "";
-        resultadoCrudo = null;
+        totalregistros = 0;
     }
-
-    //protected ResultSet getResultadoCrudo() { return resultadoCrudo; }
-
-    protected void consultar(){
-        resultadoCrudo = null;
-        try {
-            Statement s = con.Crear();
-            s.execute(cadSQL);
-            resultadoCrudo = s.getResultSet();
-            con.Cerrar();
-        } catch (Exception e) { resultadoCrudo = null; }
-    }
-
-    protected DefaultTableModel obtenerTabla(String encabezado[]){
-        DefaultTableModel ret = null;
-        try {
-            consultar();
-            String regs[] = new String[encabezado.length];
-            while(resultadoCrudo.next()){
-                for (int i = 0; i < encabezado.length; i++) regs[i] = resultadoCrudo.getString(encabezado[i]);
-                ret.addRow(regs);
-            }
-            resultadoCrudo.first();
-        } catch (Exception e) { ret = null; }
-        return ret;
-    }
-
+    
     public abstract DefaultTableModel mostrar();
     public abstract boolean insertar(Datos dts);
     public abstract boolean editar(Datos dts);
